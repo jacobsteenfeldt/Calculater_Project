6c55768d35065a0e449abab376b25aa8c0a13fc4
Jacob <jacobsteenfeldt@gmail.com>
2023-12-06 12:41:36 +0100

    Added the final calculation methods

--- Calculator_Project/Calculater.cs
+++ Calculator_Project/Calculater.cs
@@ -12,8 +12,11 @@ public class Calculator
     public int multiply(int a, int b)
     { return a * b; }
 
-    public int divide(int a, int b)
-    { return a / b; }
+    public float divide(int a, int b)
+    {
+        float calc = Convert.ToSingle(a)/Convert.ToSingle(b);
+        return calc;
+    }
 
     public void run()
     {
@@ -30,5 +33,38 @@ public class Calculator
             Console.WriteLine("The sum of your numbers are " + add(a, b));
             Console.ReadLine();
         }
+        else if (answer == "subtract")
+        {
+            Console.WriteLine("Perfect then please enter your first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("and then your second number");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("The correct answer is " + subtract(a, b));
+            Console.ReadLine();
+        }
+        else if (answer == "multiply")
+        {
+            Console.WriteLine("Perfect then please enter your first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("and then your second number");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("The correct answer is " + multiply(a, b));
+            Console.ReadLine();
+        }
+        else if (answer == "divide")
+        {
+            Console.WriteLine("Perfect then please enter your first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("and then your second number");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("The correct answer is ");
+            Console.WriteLine(divide(a, b));
+            Console.ReadLine();
+        }
+    }
+
+    public void rerun()
+    {
+
     }
 }
